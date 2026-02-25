1e0259a9b3b664ae830137b69595750eb8f1d5e1
mphintener <74789765+mphintener@users.noreply.github.com>
2026-02-25 14:19:43 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -152,7 +152,7 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-f st.button("GERAR DIAGNÓSTICO"):
+if st.button("GERAR DIAGNÓSTICO"):
     # Verificação amigável
     if salario is None or h_dia is None or salario == 0:
         st.warning("⚠️ Por favor, preencha os campos de SALÁRIO, GASTO COM TRANSPORTE E HORAS NO TRECHO para continuar.")
